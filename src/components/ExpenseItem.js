eb53e32769b5bbe7eb15de99743eab5b66ba4252
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-06-05 23:23:06 +0300

    props ile data transfer edildi. props ile className ' deki css class ' ı wrapper elemente geçildi. props.children özelliği ile wrapper elementi oluşturuldu.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate month={month} year={year} day={day} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
