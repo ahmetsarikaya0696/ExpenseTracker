b8168a3a65376776c559bd694bd4b0c4de2efa5c
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-06-06 21:16:57 +0300

    Headline elementi oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import Headline from "./components/Headline/Headline";
 
 const DUMMY_EXPENSES = [
   { id: "e1", title: "Pencil", amount: 94.12, date: new Date(2020, 7, 14) },
@@ -23,6 +24,7 @@ const App = () => {
 
   return (
     <div>
+      <Headline />
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
---
+++ src/components/Headline/Headline.js
@@ -0,0 +1,12 @@
+import "./Headline.css";
+import Card from "../UI/Card";
+
+const Headline = () => {
+  return (
+    <Card className="headline">
+        <h1>Expense Tracker App</h1>
+    </Card>
+  );
+};
+
+export default Headline;
