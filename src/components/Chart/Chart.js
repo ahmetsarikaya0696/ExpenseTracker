b64f06d139dcf507d7470ef772e1c1c108f10e5c
ahmet sarikaya <ahmetsarikaya0696@outlook.com>
2023-06-07 22:56:30 +0300

    ChartBar tamamlandı

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,11 +1,22 @@
+import React from "react";
 import Card from "../UI/Card";
 import "./Chart.css";
 import ChartBar from "./ChartBar";
 
-const Chart = () => {
+const Chart = (props) => {
+  const amounts = props.dataPoints.map(dataPoint => (dataPoint.value));
+  const total = amounts.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
   return (
     <Card className="chart">
-        <ChartBar month={month} />
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          total={total}
+          label={dataPoint.label}
+        />
+      ))}
     </Card>
   );
 };
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,12 +1,22 @@
+import React from "react";
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
+  let barFillPercentage = "0%";
+  if (props.total > 0) {
+    console.log("value : " + props.value + " tip : " + typeof(props.value));
+    console.log("total : " + props.total + " tip : " + typeof(props.value));
+    console.log(Math.round(props.value / props.total).toFixed(2));
+    barFillPercentage =  Math.round(props.value / props.total * 100) + '%';
+  }
+
+  console.log(barFillPercentage);
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill"></div>
+        <div style={{ height: barFillPercentage }} className="chart-bar__fill" ></div>
       </div>
-      <label className="chart-bar__label">{props.month}</label>
+      <label className="chart-bar__label">{props.label}</label>
     </div>
   );
 };
